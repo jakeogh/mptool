b2861a2c8eec274d15fe2c64749b2ecfb4972e88
jakeogh <github.com@v6y.net>
2022-06-24 09:33:50 -0700

    auto-commit

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 import fastentrypoints
 
 if not sys.version_info[0] == 3:
-    sys.exit("Python 3 is required. Use: \'python3 setup.py install\'")
+    sys.exit("Python 3 is required. Use: 'python3 setup.py install'")
 
 dependencies = ["icecream", "click"]
 
@@ -21,8 +21,8 @@ config = {
     "author_email": "github.com@v6y.net",
     "description": "messagepack functions for typed CLI IPC",
     "long_description": __doc__,
-    "packages": find_packages(exclude=['tests']),
-    "package_data": {"mptool": ['py.typed']},
+    "packages": find_packages(exclude=["tests"]),
+    "package_data": {"mptool": ["py.typed"]},
     "include_package_data": True,
     "zip_safe": False,
     "platforms": "any",
