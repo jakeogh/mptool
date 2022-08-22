12c4cff63bac7c725d13f2b6b247769039a8b385
jakeogh <github.com@v6y.net>
2022-08-21 20:20:49 -0700

    auto-commit

--- mptool/mptool.py
+++ mptool/mptool.py
@@ -5,66 +5,12 @@
 from __future__ import annotations
 
 import sys
-# from collections.abc import Iterator
 from math import inf
 from typing import Any
 from typing import BinaryIO
 
 import msgpack
 from epprint import epprint
-
-# from typing import Type
-
-
-
-## verbose can be math.inf
-# def unmp(
-#    *,
-#    verbose: bool | int | float,
-#    valid_types: None | list | tuple = None,
-#    buffer_size: int = 1024,
-#    skip: None | int = None,
-#    single_type: bool = True,
-#    strict_map_key: bool = False,  # True is the default
-#    file_handle: BinaryIO = sys.stdin.buffer,
-# ) -> Iterator[object]:
-#
-#    unpacker = msgpack.Unpacker(strict_map_key=strict_map_key, use_list=False)
-#    index = 0
-#    if valid_types:
-#        for _type in valid_types:
-#            if not isinstance(_type, Type):
-#                raise ValueError(
-#                    f"valid_types was passed with a non-Type member {_type=}"
-#                )
-#
-#    found_type: Type = type(None)
-#    for chunk in iter(lambda: file_handle.read(buffer_size), b""):
-#        if verbose == inf:
-#            epprint(
-#                f"{valid_types=}",
-#                f"{buffer_size=}",
-#                f"{type(chunk)=}," f"{len(chunk)=}",
-#                f"{chunk=}",
-#            )
-#        unpacker.feed(chunk)
-#        for value in unpacker:
-#            if single_type:
-#                if index == 0:
-#                    found_type = type(value)
-#                else:
-#                    if not isinstance(value, found_type):
-#                        raise TypeError(f"{value=} does not match {found_type=}")
-#            index += 1
-#            if verbose == inf:
-#                epprint(f"{index=}", f"{value=}")
-#            if skip is not None:
-#                if index <= skip:
-#                    continue
-#            if valid_types is not None:
-#                if type(value) not in valid_types:
-#                    raise TypeError(f"{type(value)} not in valid_types: {valid_types}")
-#            yield value
 
 
 def _output(
@@ -131,7 +77,7 @@ def output(
     arg,
     *,
     reason: Any,
-    dict_input: bool,
+    dict_output: bool,
     tty: bool,
     verbose: bool | int | float,
     stderr: bool = False,
@@ -140,7 +86,7 @@ def output(
     file_handle_encoding: None | str = None,
 ) -> None:
 
-    if dict_input:
+    if dict_output:
         _arg = {reason: arg}
     else:
         _arg = arg
